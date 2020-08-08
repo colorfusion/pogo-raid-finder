eff440d24452412d87280d0a1281c09457a277b2
Melvin Yeo <1024128+colorfusion@users.noreply.github.com>
2020-08-08 13:42:56 +0800

    Add initial data for app

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <v-card>
+        <v-card-title class="headline">
+          Pogo Raid Finder
+        </v-card-title>
+        <v-card-text>
+          <p>
+            Insert some intro text here that has not much meaning right now.
+          </p>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="primary" nuxt to="/raids">
+            Continue
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script></script>
