eca88332b2fa9ecbe5f1531031cde4ed67e8e831
Melvin Yeo <1024128+colorfusion@users.noreply.github.com>
2020-08-27 00:36:15 +0800

    Fix lint errors

--- pages/lobby.vue
+++ pages/lobby.vue
@@ -3,12 +3,12 @@
     <v-col class="mb-6" cols="12" md="8">
       <v-row>
         <v-col cols="12">
-          <v-skeleton-loader type="image" height="8em" boilerplate="true" />
+          <v-skeleton-loader type="image" height="8em" boilerplate />
         </v-col>
       </v-row>
       <v-row>
         <v-col v-for="n in 2" :key="n" cols="6">
-          <v-skeleton-loader type="image" height="4em" boilerplate="true" />
+          <v-skeleton-loader type="image" height="4em" boilerplate />
         </v-col>
       </v-row>
       <v-row v-for="n in 6" :key="n">
